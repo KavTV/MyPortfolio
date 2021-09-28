53adede8b99a943f53b9315b45f9335e82b5de51
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2021-09-28 14:14:34 +0200

    Thirdperson camera, generating path & "car" moving following path/road

--- main.js
+++ main.js
@@ -1,43 +1,90 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Vector3 } from 'three';
+// import { InstancedFlow } from './jsm/modifiers/CurveModifier.js';
 
 
-class ThirdPersonCamera{
-  constructor(params){
-    this._params = params;
-    this._camera = params.camera;
-    this._currentPosition = new THREE.Vector3();
-    this._currentLookat = new THREE.Vector3();
+class ThirdPersonCamera {
+
+  Update(geometry) {
+    var temp = new THREE.Vector3;
+
+    //Set offset
+    temp.y = geometry.position.y + 10;
+    temp.z = geometry.position.z + 10;
+    temp.x = geometry.position.x;
+
+    camera.position.lerp(temp, 0.2);
   }
-  _CalculateIdealOffset() {
-    const idealOffset = new THREE.Vector3(-15, 20, -30);
-    idealOffset.applyQuaternion(this._params.target.Rotation);
-    idealOffset.add(this._params.target.Position);
-    return idealOffset;
+}
+
+class Road{
+constructor(curve){
+  this.curve = curve;
+}
+  CreateRoad(){
+    const points = this.curve.getPoints(50);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    
+    const linematerial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    
+    //Draw a line from the created road
+    const curveObject = new THREE.Line(geometry, linematerial);
+    scene.add(curveObject);
+    
+    //Settings for the road
+    var extrudeSettings = {
+      steps: 100,
+      bevelEnabled: true,
+      extrudePath: this.curve,
+      bevelThickness: 0
+    };
+    
+    
+    var shape = new THREE.Shape();
+    shape.moveTo(0, 0);
+    shape.lineTo(0,-5);
+    shape.lineTo(0, 5);
+    
+    // Extrude the triangle along the CatmullRom curve
+    var geometry2 = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    var material2 = new THREE.MeshLambertMaterial({ color: 0xb00000, wireframe: false });
+    
+    // Create mesh with the resulting geometry
+    var mesh = new THREE.Mesh(geometry2, material2);
+    
+    //Add road
+    scene.add(mesh);
   }
+}
 
-  _CalculateIdealLookat() {
-    const idealLookat = new THREE.Vector3(0, 10, 50);
-    idealLookat.applyQuaternion(this._params.target.Rotation);
-    idealLookat.add(this._params.target.Position);
-    return idealLookat;
-  }
+class Car{
+constructor(car){
+  this.car = car;
+  this.carPosition = new Vector3();
+  scene.add(this.car);
+}
+MoveCarOnRoad(curveRoad){
+  const time = performance.now();
+  //Find the delta time
+  const delta = time - prevTime;
 
-  Update(timeElapsed) {
-    const idealOffset = this._CalculateIdealOffset();
-    const idealLookat = this._CalculateIdealLookat();
+  progress += velocity;
+  progress = progress % 1;
 
-    // const t = 0.05;
-    // const t = 4.0 * timeElapsed;
-    const t = 1.0 - Math.pow(0.001, timeElapsed);
+  car.carPosition.copy(curveRoad.getPointAt(progress));
+  car.carPosition.y += 0.3;
 
-    this._currentPosition.lerp(idealOffset, t);
-    this._currentLookat.lerp(idealLookat, t);
+  car.car.position.copy(car.carPosition);
 
-    this._camera.position.copy(this._currentPosition);
-    this._camera.lookAt(this._currentLookat);
-  }
+  tangent.copy(curveRoad.getTangentAt(progress));
+
+  velocity -= tangent.y * 0.0000001 * delta;
+  velocity = Math.max(0.0005, Math.min(0.0002, velocity));
+  car.car.lookAt(lookAt.copy(car.carPosition).sub(tangent));
+}
+  
 }
 
 
@@ -45,10 +92,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-this._thirdPersonCamera = new ThirdPersonCamera({
-  camera: this._camera,
-  target: this._controls,
-});
+var thirdPersonCamera = new ThirdPersonCamera();
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -56,7 +100,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(50);
+camera.position.setZ(40);
 camera.position.setY(20);
 
 
@@ -66,18 +110,21 @@ renderer.render(scene, camera);
 
 // Geometry
 
-const planeGeometry = new THREE.PlaneGeometry(30,40);
+const planeGeometry = new THREE.PlaneGeometry(30, 40);
 const material = new THREE.MeshPhongMaterial({ color: 0xff6347 });
 const GroundPlane = new THREE.Mesh(planeGeometry, material);
 GroundPlane.position.setY(-0.1);
 GroundPlane.rotation.x = Math.PI * -.5;
 
-const carGeometry = new THREE.BoxGeometry(2,2);
-const carMaterial = new THREE.MeshBasicMaterial({color: 0xfc0303})
-const car = new THREE.Mesh(carGeometry, carMaterial);
+//Car 
+let carGeometry = new THREE.BoxGeometry(2, 2);
+let carMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+let carMesh = new THREE.Mesh(carGeometry, carMaterial);
 
-scene.add(GroundPlane);
-scene.add(car);
+const car = new Car(carMesh); 
+
+// scene.add(GroundPlane);
+const carPosition = new THREE.Vector3();
 
 // Lights
 
@@ -91,12 +138,41 @@ scene.add(pointLight, ambientLight);
 
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add( gridHelper)
+scene.add(gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.rotation.x = -0.7;
 
-function moveOnScroll(){
+
+//#region  ROAD
+
+
+//Create the vectors (points) for the curved road
+const curve = new THREE.CatmullRomCurve3([
+  new THREE.Vector3(0, 0, 0),
+  new THREE.Vector3(10, 0, 20),
+  new THREE.Vector3(-10, 0, 40),
+  new THREE.Vector3(10, 0, 60),
+  new THREE.Vector3(-10, 0, 80),
+  new THREE.Vector3(10, 0, 100)
+]);
+curve.type = 'chordal';
+curve.closed = false;
+
+//Create a road from the curve
+var road = new Road(curve);
+road.CreateRoad(curve);
+
+
+//#endregion
+
+
+//#region   ---EVENTS---
+document.body.onscroll = moveOnScroll;
+
+//#endregion
+//#region   ---FUNCTIONS---
+function moveOnScroll() {
 
   const t = document.body.getBoundingClientRect().top;
 
@@ -104,34 +180,28 @@ function moveOnScroll(){
 
 }
 
-//EVENTS
-document.body.onscroll = moveOnScroll;
+//Used for moving the car.
+const tangent = new THREE.Vector3();
 
+const lookAt = new THREE.Vector3();
 
-function _Step(timeElapsed) {
-  const timeElapsedS = timeElapsed * 0.001;
-  if (this._mixers) {
-    this._mixers.map(m => m.update(timeElapsedS));
-  }
-
-  this._thirdPersonCamera.Update(timeElapsedS);
-}
-
-this._previousRAF = null;
+let velocity = 0;
+let progress = 0;
+let prevTime = performance.now();
 
 function animate() {
-  requestAnimationFrame((t) => {
-    if(this._previousRAF === null){
-      this._previousRAF = t;
-    }
+  requestAnimationFrame(animate);
 
-    this.animate();
+  //Move the car on road
+  car.MoveCarOnRoad(road.curve);
 
+  renderer.render(scene, camera);
 
-    renderer.render(scene, camera);
-    this.thirdPersonCamera.Update()
-
-  });
+  //Make the camera follow the car.
+  thirdPersonCamera.Update(car.car)
 }
+//#endregion
 
 animate();
+
+
