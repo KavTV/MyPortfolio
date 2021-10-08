37ec785906d39eee1766e2ed5369e81cccceae47
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2021-10-08 12:45:13 +0200

    Added shadows, a tunnel, and moved all classes to seperate file

---
+++ classes.js
@@ -0,0 +1,245 @@
+import * as THREE from 'three';
+import { Vector3 } from 'three';
+
+export class ThirdPersonCamera {
+  constructor(camera) {
+    this.camera = camera;
+  }
+  Update(geometry) {
+    var temp = new THREE.Vector3;
+
+    //Set offset
+    temp.y = geometry.position.y + 10;
+    temp.z = geometry.position.z + 10;
+    temp.x = geometry.position.x;
+
+    this.camera.position.lerp(temp, 0.2);
+  }
+  }
+  export class Road {
+    constructor(imageArray, scene) {
+      this.curve;
+      this.scene = scene;
+      this.CreateRoad(imageArray);
+      this.DrawRoad();
+    }
+  
+    CreateRoad(imageArray){
+      //Determines if the curve is gonna be left or right
+      let isLeft = false;
+      //Start an array for the curve.
+      let vectorArray = [new THREE.Vector3(0,0,0)];
+  
+      //Randomly generate curves for the road
+      let roadLength = 25;
+      for (let i = 1; i <= imageArray.length; i++) {
+  
+        let vector;
+        //Add a sign on the side of the road
+        let sign = this.CreateSign(6, imageArray[i-1]);
+  
+        if (isLeft == false) {
+          vector = new THREE.Vector3(THREE.Math.randFloat(5, 20), 0, i * roadLength);
+          vectorArray.push(vector);
+  
+          sign.position.set(vector.x - 10 ,0,vector.z);
+  
+          isLeft = true;
+        }
+        else {
+          vector = new THREE.Vector3(THREE.Math.randFloat(-5, -20), 0, i * roadLength);
+          vectorArray.push(vector);
+  
+          sign.position.set(vector.x + 10,0,vector.z);
+  
+          isLeft = false;
+        }
+        
+      }
+      //Make the last bit of the road going center.
+      vectorArray.push(new THREE.Vector3(0,0,(imageArray.length+1) * roadLength));
+      console.log(vectorArray);
+  
+      //Create the curve from the generated vectors
+       const curve = new THREE.CatmullRomCurve3(
+        vectorArray
+      );
+      curve.type = 'chordal';
+      curve.closed = false;
+      //Set the curve
+      this.curve = curve;
+  
+    }
+    DrawRoad() {
+      const points = this.curve.getPoints(80);
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+  
+      const linematerial = new THREE.LineBasicMaterial({ color: 0xffffff });
+  
+      //Draw a line from the created road
+      const curveLine = new THREE.Line(geometry, linematerial);
+      this.scene.add(curveLine);
+  
+      //Settings for the road
+      var extrudeSettings = {
+        steps: 100,
+        bevelEnabled: true,
+        extrudePath: this.curve,
+        bevelThickness: 0
+      };
+  
+  
+      var shape = new THREE.Shape();
+      shape.moveTo(0, 0);
+      shape.lineTo(0, -5);
+      shape.lineTo(0, 5);
+  
+      // Extrude the path along the CatmullRom curve
+      var pathGeometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+      var pathMaterial = new THREE.MeshPhongMaterial({ color: 0x46494d, wireframe: false });
+  
+      // Create mesh with the resulting geometry
+      var mesh = new THREE.Mesh(pathGeometry, pathMaterial);
+  
+      mesh.castShadow = false;
+      mesh.receiveShadow = true;
+
+      //Add road
+      this.scene.add(mesh);
+    }
+  
+    CreateSign(height, imageLocation) {
+      //Group the objects for the sign
+      let sign = new THREE.Group();
+      //The picture on the sign.
+      let planeGeometry = new THREE.PlaneGeometry(10, 5);
+      let planetexture = new THREE.TextureLoader().load(imageLocation);
+      let planeMaterial = new THREE.MeshBasicMaterial({ map: planetexture });
+      let SignPlane = new THREE.Mesh(planeGeometry, planeMaterial);
+      let SignPlaneBack = new THREE.Mesh(planeGeometry, planeMaterial);
+  
+      //Point it backwards
+      SignPlaneBack.rotateY(Math.PI * 1);
+  
+      SignPlane.position.y = height;
+      SignPlaneBack.position.y = height;
+  
+      SignPlane.castShadow = true;
+      SignPlane.receiveShadow = true;
+      SignPlaneBack.castShadow = true;
+      SignPlaneBack.receiveShadow = true;
+
+      sign.add(SignPlane);
+      sign.add(SignPlaneBack);
+  
+      //Add some pillars that hold the sign.
+      let pillar1 = this.CreatePillar(height - SignPlane.geometry.parameters.height/2);
+      let pillar2 = this.CreatePillar(height - SignPlane.geometry.parameters.height/2);
+  
+      //Calculate the position of the pillars for compatability with different sizes.
+      let pillarOffset = 2;
+      pillar1.position.x = -SignPlane.geometry.parameters.width/2 + pillarOffset;
+      pillar2.position.x = SignPlane.geometry.parameters.width/2 - pillarOffset;
+  
+      sign.add(pillar1);
+      sign.add(pillar2);
+  
+      //Add a frame
+      let frame = this.CreateFrame(SignPlane);
+      // SignPlane.rotation.y = Math.PI * .1;
+      sign.add(frame);
+      this.scene.add(sign);
+  
+      return sign;
+    }
+    CreatePillar(height) {
+      //Add the pillar for sign.
+      let pillarGeometry = new THREE.CylinderGeometry(.2, .2, height, 32);
+      let pillarMaterial = new THREE.MeshPhongMaterial({ color: 0x4d5157 });
+      let pillar = new THREE.Mesh(pillarGeometry, pillarMaterial);
+      pillar.castShadow = true;
+      pillar.receiveShadow = true;
+      pillar.position.y = height/2;
+      return pillar;
+    }
+    CreateFrame(plane){
+      let frame = new THREE.Group();
+      //Get the height and size of the plane
+      let planeSize = plane.geometry.parameters;
+  
+      //Add a frame to the plane.
+      let topFrame = this.CreateFrameBox(planeSize.width + .1, .1);
+      let bottomFrame = this.CreateFrameBox(planeSize.width + .1, .1);
+      let leftFrame = this.CreateFrameBox(.1, planeSize.height);
+      let rightFrame = this.CreateFrameBox(.1, planeSize.height);
+  
+      //Calculate and move the frames to correct position.
+      //Top/Bottom frames
+      topFrame.position.y = plane.position.y + planeSize.height/2;
+      bottomFrame.position.y = plane.position.y + -planeSize.height/2;
+      //Side frames
+      leftFrame.position.y = plane.position.y;
+      rightFrame.position.y = plane.position.y;
+      leftFrame.position.x = -planeSize.width/2;
+      rightFrame.position.x = planeSize.width/2;
+  
+      frame.add(topFrame);
+      frame.add(bottomFrame);
+      frame.add(leftFrame);
+      frame.add(rightFrame);
+      return frame;
+    }
+  
+    CreateFrameBox(width, height){
+      let frameGeometry = new THREE.BoxGeometry(width,height,.3);
+      let frameMaterial = new THREE.MeshPhongMaterial({ color: 0x4d5157 });
+      let frameMesh = new THREE.Mesh(frameGeometry, frameMaterial);
+      return frameMesh;
+    }
+  
+  }
+
+  export class Car {
+    constructor(car, scene) {
+      this.car = car;
+      this.carPosition = new Vector3();
+      
+      //Used for moving the car.
+      this.tangent = new THREE.Vector3();
+
+      this.lookAt = new THREE.Vector3();
+
+      this.velocity = 0;
+      this.progress = 0;
+      this.prevTime = performance.now();
+
+      this.car.castShadow = true;
+      this.car.receiveShadow = true;
+
+      scene.add(this.car);
+    }
+    MoveCarOnRoad(curveRoad) {
+      const time = performance.now();
+      //Find the delta time
+      const delta = time - this.prevTime;
+  
+      this.progress += this.velocity;
+      this.progress = this.progress % 1;
+  
+      this.carPosition.copy(curveRoad.getPointAt(this.progress));
+      //Offset for car height
+      this.carPosition.y += 0.7;
+      //Offset for the side of the road
+      this.carPosition.x += -3;
+  
+      this.car.position.copy(this.carPosition);
+  
+      this.tangent.copy(curveRoad.getTangentAt(this.progress));
+
+      this.velocity -= this.tangent.y * 0.0000001 * delta;
+      this.velocity = Math.max(0.0005, Math.min(0.0002, this.velocity));
+      //Turn model in direction of the path
+      this.car.lookAt(this.lookAt.copy(this.carPosition).sub(this.tangent));
+    }
+  
+  }
--- main.js
+++ main.js
@@ -3,223 +3,13 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {Road, ThirdPersonCamera, Car} from './classes.js';
 
 
-class ThirdPersonCamera {
-
-  Update(geometry) {
-    var temp = new THREE.Vector3;
-
-    //Set offset
-    temp.y = geometry.position.y + 10;
-    temp.z = geometry.position.z + 10;
-    temp.x = geometry.position.x;
-
-    camera.position.lerp(temp, 0.2);
-  }
-}
-
-class Road {
-  constructor(imageArray) {
-    this.curve;
-    this.CreateRoad(imageArray);
-    this.DrawRoad();
-  }
-
-  CreateRoad(imageArray){
-    //Determines if the curve is gonna be left or right
-    let isLeft = false;
-    //Start an array for the curve.
-    let vectorArray = [new THREE.Vector3(0,0,0)];
-
-    //Randomly generate curves for the road
-    let roadLength = 25;
-    for (let i = 1; i <= imageArray.length; i++) {
-
-      let vector;
-      //Add a sign on the side of the road
-      let sign = this.CreateSign(6, imageArray[i-1]);
-
-      if (isLeft == false) {
-        vector = new THREE.Vector3(THREE.Math.randFloat(5, 20), 0, i * roadLength);
-        vectorArray.push(vector);
-
-        sign.position.set(vector.x - 10 ,0,vector.z);
-
-        isLeft = true;
-      }
-      else {
-        vector = new THREE.Vector3(THREE.Math.randFloat(-5, -20), 0, i * roadLength);
-        vectorArray.push(vector);
-
-        sign.position.set(vector.x + 10,0,vector.z);
-
-        isLeft = false;
-      }
-      
-    }
-    console.log(vectorArray);
-
-    //Create the curve from the generated vectors
-     const curve = new THREE.CatmullRomCurve3(
-      vectorArray
-    );
-    curve.type = 'chordal';
-    curve.closed = false;
-    //Set the curve
-    this.curve = curve;
-
-  }
-  DrawRoad() {
-    const points = this.curve.getPoints(80);
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    const linematerial = new THREE.LineBasicMaterial({ color: 0xffffff });
-
-    //Draw a line from the created road
-    const curveLine = new THREE.Line(geometry, linematerial);
-    scene.add(curveLine);
-
-    //Settings for the road
-    var extrudeSettings = {
-      steps: 100,
-      bevelEnabled: true,
-      extrudePath: this.curve,
-      bevelThickness: 0
-    };
 
 
-    var shape = new THREE.Shape();
-    shape.moveTo(0, 0);
-    shape.lineTo(0, -5);
-    shape.lineTo(0, 5);
 
-    // Extrude the path along the CatmullRom curve
-    var pathGeometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-    var pathMaterial = new THREE.MeshLambertMaterial({ color: 0x46494d, wireframe: false });
 
-    // Create mesh with the resulting geometry
-    var mesh = new THREE.Mesh(pathGeometry, pathMaterial);
-
-    //Add road
-    scene.add(mesh);
-  }
-
-  CreateSign(height, imageLocation) {
-    //Group the objects for the sign
-    let sign = new THREE.Group();
-    //The picture on the sign.
-    let planeGeometry = new THREE.PlaneGeometry(7, 4);
-    let planetexture = new THREE.TextureLoader().load(imageLocation);
-    let planeMaterial = new THREE.MeshPhongMaterial({ map: planetexture });
-    let SignPlane = new THREE.Mesh(planeGeometry, planeMaterial);
-    let SignPlaneBack = new THREE.Mesh(planeGeometry, planeMaterial);
-
-    //Point it backwards
-    SignPlaneBack.rotateY(Math.PI * 1);
-
-    SignPlane.position.y = height;
-    SignPlaneBack.position.y = height;
-
-    sign.add(SignPlane);
-    sign.add(SignPlaneBack);
-
-    //Add some pillars that hold the sign.
-    let pillar1 = this.CreatePillar(height - SignPlane.geometry.parameters.height/2);
-    let pillar2 = this.CreatePillar(height - SignPlane.geometry.parameters.height/2);
-
-    //Calculate the position of the pillars for compatability with different sizes.
-    let pillarOffset = 2;
-    pillar1.position.x = -SignPlane.geometry.parameters.width/2 + pillarOffset;
-    pillar2.position.x = SignPlane.geometry.parameters.width/2 - pillarOffset;
-
-    sign.add(pillar1);
-    sign.add(pillar2);
-
-    //Add a frame
-    let frame = this.CreateFrame(SignPlane);
-    // SignPlane.rotation.y = Math.PI * .1;
-    sign.add(frame);
-    scene.add(sign);
-
-    return sign;
-  }
-  CreatePillar(height) {
-    //Add the pillar for sign.
-    let pillarGeometry = new THREE.CylinderGeometry(.2, .2, height, 32);
-    let pillarMaterial = new THREE.MeshPhongMaterial({ color: 0x4d5157 });
-    let pillar = new THREE.Mesh(pillarGeometry, pillarMaterial);
-    pillar.position.y = height/2;
-    return pillar;
-  }
-  CreateFrame(plane){
-    let frame = new THREE.Group();
-    //Get the height and size of the plane
-    let planeSize = plane.geometry.parameters;
-
-    //Add a frame to the plane.
-    let topFrame = this.CreateFrameBox(planeSize.width + .1, .1);
-    let bottomFrame = this.CreateFrameBox(planeSize.width + .1, .1);
-    let leftFrame = this.CreateFrameBox(.1, planeSize.height);
-    let rightFrame = this.CreateFrameBox(.1, planeSize.height);
-
-    //Calculate and move the frames to correct position.
-    //Top/Bottom frames
-    topFrame.position.y = plane.position.y + planeSize.height/2;
-    bottomFrame.position.y = plane.position.y + -planeSize.height/2;
-    //Side frames
-    leftFrame.position.y = plane.position.y;
-    rightFrame.position.y = plane.position.y;
-    leftFrame.position.x = -planeSize.width/2;
-    rightFrame.position.x = planeSize.width/2;
-
-    frame.add(topFrame);
-    frame.add(bottomFrame);
-    frame.add(leftFrame);
-    frame.add(rightFrame);
-    return frame;
-  }
-
-  CreateFrameBox(width, height){
-    let frameGeometry = new THREE.BoxGeometry(width,height,.3);
-    let frameMaterial = new THREE.MeshPhongMaterial({ color: 0x4d5157 });
-    let frameMesh = new THREE.Mesh(frameGeometry, frameMaterial);
-    return frameMesh;
-  }
-
-}
-
-class Car {
-  constructor(car) {
-    this.car = car;
-    this.carPosition = new Vector3();
-    scene.add(this.car);
-  }
-  MoveCarOnRoad(curveRoad) {
-    const time = performance.now();
-    //Find the delta time
-    const delta = time - prevTime;
-
-    progress += velocity;
-    progress = progress % 1;
-
-    car.carPosition.copy(curveRoad.getPointAt(progress));
-    //Offset for car height
-    car.carPosition.y += 0.7;
-    //Offset for the side of the road
-    car.carPosition.x += -3;
-
-    car.car.position.copy(car.carPosition);
-
-    tangent.copy(curveRoad.getTangentAt(progress));
-
-    velocity -= tangent.y * 0.0000001 * delta;
-    velocity = Math.max(0.0005, Math.min(0.0002, velocity));
-    //Turn model in direction of the path
-    car.car.lookAt(lookAt.copy(car.carPosition).sub(tangent));
-  }
-
-}
 
 //#region INIT
 const scene = new THREE.Scene();
@@ -227,7 +17,7 @@ const scene = new THREE.Scene();
 //Make camera with windows aspect ratio
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var thirdPersonCamera = new ThirdPersonCamera();
+var thirdPersonCamera = new ThirdPersonCamera(camera);
 
 const modelLoader = new GLTFLoader();
 
@@ -238,6 +28,10 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 //Match window size.
 renderer.setSize(window.innerWidth, window.innerHeight);
+//Shadows
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 camera.position.setZ(40);
 camera.position.setY(20);
 
@@ -251,9 +45,18 @@ camera.position.setY(20);
 // Geometry
 
 //The ground
-const planeGeometry = new THREE.PlaneGeometry(100, 300);
-const material = new THREE.MeshPhongMaterial({ color: 0xff6347 });
-const GroundPlane = new THREE.Mesh(planeGeometry, material);
+const planeGeometry = new THREE.PlaneGeometry(300, 500);
+let forestTexture = new THREE.TextureLoader().load('Materials/forest-floor/forest_floor_albedo.png');
+forestTexture.wrapS = THREE.RepeatWrapping;
+forestTexture.wrapT = THREE.RepeatWrapping;
+forestTexture.repeat.set(7,7);
+
+const forestMaterial = new THREE.MeshLambertMaterial({ map:  forestTexture});
+
+const GroundPlane = new THREE.Mesh(planeGeometry, forestMaterial);
+
+GroundPlane.receiveShadow = true;
+GroundPlane.castShadow = true;
 
 GroundPlane.position.setY(-0.1);
 GroundPlane.rotation.x = Math.PI * -.5;
@@ -263,43 +66,69 @@ GroundPlane.rotation.x = Math.PI * -.5;
 var car;
 // let carMesh;
 modelLoader.load('car.glb', function (gltf) {
-  car = new Car(gltf.scene);
-})
+  //Make all objects cast shadow
+  gltf.scene.traverse( function( node ) {
+
+    if ( node.isMesh ) { node.castShadow = true; }
+
+} );
+  car = new Car(gltf.scene, scene);
+});
+
+
+
 
 
 scene.add(GroundPlane);
 
-const carPosition = new THREE.Vector3();
-
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+const spotLight = new THREE.SpotLight(0xffffff, 2);
+spotLight.position.set(200, 100, 350);
+spotLight.castShadow = true;
+// spotLight.shadow.bias = -0.0001;
+spotLight.shadow.mapSize.width = 1024*5
+spotLight.shadow.mapSize.height = 1024*5
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add( spotLight, ambientLight);
 
 // Helpers
 
-const axesHelper = new THREE.AxesHelper(20);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(20);
+// scene.add(axesHelper);
 
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper)
+// const lightHelper = new THREE.SpotLightHelper(spotLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 camera.rotation.x = -0.7;
 
 
 //#region  ROAD
 
 //Create a road from the curve
-let testArray = ['TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png']
-var road = new Road(testArray);
+let testArray = ['TeleTavleHelper.png','RandomSixOnline.png','TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png','TeleTavleHelper.png']
+var road = new Road(testArray, scene);
 
 //#endregion
 
+var tunnel;
+modelLoader.load('tunnel.glb', function (gltf) {
+  //Set the position the the tunnel
+  tunnel = gltf.scene
+  tunnel.position.set(0,4,0);
+  let tunnelRotation = road.curve.getTangentAt(.003);
+  //Make sure the tunnel just faces the direction, not the y position, which makes it point down.
+  tunnelRotation.y = tunnel.position.y;
+  tunnel.lookAt(tunnelRotation);
+
+  tunnel.castShadow = true;
+  tunnel.receiveShadow = true;
+
+  scene.add(tunnel);
+});
 
 //#region   ---EVENTS---
 document.body.onscroll = moveOnScroll;
@@ -314,20 +143,13 @@ function moveOnScroll() {
 
 }
 
-//Used for moving the car.
-const tangent = new THREE.Vector3();
 
-const lookAt = new THREE.Vector3();
-
-let velocity = 0;
-let progress = 0;
-let prevTime = performance.now();
 
 function animate() {
   requestAnimationFrame(animate);
 
   //Move the car on road
-  if (car) {
+  if (car && thirdPersonCamera) {
     car.MoveCarOnRoad(road.curve);
     thirdPersonCamera.Update(car.car)
   }
